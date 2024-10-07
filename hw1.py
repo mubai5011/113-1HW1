557c5222001aa0e884bc10e73e7367aed23f07b2
Mubai5011  <eli.clark2124@gmail.com>
2024-10-07 15:42:15 +0800

    hw1.py

--- hw1.py
+++ hw1.py
@@ -1,23 +1,23 @@
 from typing import List
 
 def countLetters(sentence: str) -> List[int]:
-    letterCount: List[int] = [0] * 26
+    letterCount: List[int] = [0] * 26 # 初始化一個長度為26的列表，對應著每個字母的計數
 
-    for char in sentence:
-        if char.isalpha():
-            index = ord(char) - ord('a')
-            letterCount[index] += 1
+    for char in sentence: # 遍歷句子中的每個字母
+        if char.isalpha(): # 檢查字符是否為字母
+            index = ord(char) - ord('a') # 將字母轉換為對應索引(0-25)
+            letterCount[index] += 1 # 增加對應字母的計數
 
-    return letterCount
+    return letterCount # 返回字母計數列表
 pass
 
-def printLetterCount(letterCount: List[int]) -> None:
+def printLetterCount(letterCount: List[int]) -> None: # 遍歷字母計數列表
 
-    for i in range(26):
-        if letterCount[i] > 0:
+    for i in range(26): # 只打印計數大於0的字母
+        if letterCount[i] > 0: #打印字母及其計數
             print(f"{chr(i + ord('a'))}: {letterCount[i]}")
 pass
 
-inputSentence: str = "this is an apple"
-letterCount: List[int] = countLetters(inputSentence)
-printLetterCount(letterCount)
+inputSentence: str = "this is an apple" # 定義輸入的句子
+letterCount: List[int] = countLetters(inputSentence) # 計算字母的計數
+printLetterCount(letterCount) # 打印字母計數
